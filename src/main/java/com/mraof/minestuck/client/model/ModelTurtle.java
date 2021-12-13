af2004cac32c6590a1f85f8f1a990f8d74bb3b87
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2021-12-12 19:26:27 -0500

    gave iguanas and turtles joints

--- src/main/java/com/mraof/minestuck/client/model/ModelTurtle.java
+++ src/main/java/com/mraof/minestuck/client/model/ModelTurtle.java
@@ -8,13 +8,14 @@ package com.mraof.minestuck.client.model;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.MathHelper;
 
 public class ModelTurtle extends ModelBase
 {
 	// fields
 	ModelRenderer head;
-	ModelRenderer leg1;
-	ModelRenderer leg2;
+	ModelRenderer leftLeg;
+	ModelRenderer rightLeg;
 	ModelRenderer tail;
 	ModelRenderer body;
 	ModelRenderer shell;
@@ -32,18 +33,18 @@ public class ModelTurtle extends ModelBase
 		head.setTextureSize(64, 64);
 		head.mirror = true;
 		setRotation(head, 0F, 0F, 0F);
-		leg1 = new ModelRenderer(this, 32, 0);
-		leg1.addBox(-1F, 0F, -1F, 2, 4, 2);
-		leg1.setRotationPoint(1F, 20F, 1F);
-		leg1.setTextureSize(64, 64);
-		leg1.mirror = true;
-		setRotation(leg1, 0F, 0F, 0F);
-		leg2 = new ModelRenderer(this, 32, 6);
-		leg2.addBox(-1F, 0F, -1F, 2, 4, 2);
-		leg2.setRotationPoint(-2F, 20F, 1F);
-		leg2.setTextureSize(64, 64);
-		leg2.mirror = true;
-		setRotation(leg2, 0F, 0F, 0F);
+		leftLeg = new ModelRenderer(this, 32, 0);
+		leftLeg.addBox(-1F, 0F, -1F, 2, 4, 2);
+		leftLeg.setRotationPoint(1F, 20F, 1F);
+		leftLeg.setTextureSize(64, 64);
+		leftLeg.mirror = true;
+		setRotation(leftLeg, 0F, 0F, 0F);
+		rightLeg = new ModelRenderer(this, 32, 6);
+		rightLeg.addBox(-1F, 0F, -1F, 2, 4, 2);
+		rightLeg.setRotationPoint(-2F, 20F, 1F);
+		rightLeg.setTextureSize(64, 64);
+		rightLeg.mirror = true;
+		setRotation(rightLeg, 0F, 0F, 0F);
 		tail = new ModelRenderer(this, 48, 0);
 		tail.addBox(-1F, -1F, 0F, 3, 4, 1);
 		tail.setRotationPoint(-1F, 20F, 2F);
@@ -70,10 +71,11 @@ public class ModelTurtle extends ModelBase
 		setRotation(shellRim, 1.570796F, 0F, 0F);
 		nose = new ModelRenderer(this, 48, 5);
 		nose.addBox(-1F, -1F, -1F, 3, 2, 1);
-		nose.setRotationPoint(-1.1F, 10.5F, -3F);
+		nose.setRotationPoint(-0.6F, -1.5F, -4F);
 		nose.setTextureSize(64, 64);
 		nose.mirror = true;
 		setRotation(nose, 0F, 0F, 0F);
+		head.addChild(nose);
 	}
 	
 	@Override
@@ -83,13 +85,12 @@ public class ModelTurtle extends ModelBase
 		super.render(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
 		setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entity);
 		head.render(scale);
-		leg1.render(scale);
-		leg2.render(scale);
+		leftLeg.render(scale);
+		rightLeg.render(scale);
 		tail.render(scale);
 		body.render(scale);
 		shell.render(scale);
 		shellRim.render(scale);
-		nose.render(scale);
 	}
 	
 	private void setRotation(ModelRenderer model, float x, float y, float z)
@@ -97,5 +98,16 @@ public class ModelTurtle extends ModelBase
 		model.rotateAngleX = x;
 		model.rotateAngleY = y;
 		model.rotateAngleZ = z;
+	}
+
+	@Override
+	public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn)
+	{
+		float angleY = netHeadYaw / (180F / (float)Math.PI);
+		float angleX = headPitch / (180F / (float)Math.PI);
+		head.rotateAngleY = angleY;
+		head.rotateAngleX = angleX;
+		this.leftLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+		this.rightLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
 	}
 }
