f139e8d49e778125e92f28e64e46c27e71e15bc0
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2021-12-16 19:47:07 -0500

    fixed localized chat

--- src/main/java/com/mraof/minestuck/event/handlers/LocalChatEventHandler.java
+++ src/main/java/com/mraof/minestuck/event/handlers/LocalChatEventHandler.java
@@ -3,15 +3,16 @@ package com.mraof.minestuck.event.handlers;
 import com.mraof.minestuck.MinestuckConfig;
 import com.mraof.minestuck.badges.MinestuckBadges;
 import com.mraof.minestuck.capabilities.MinestuckCapabilities;
-import net.minecraft.client.Minecraft;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.server.CommandEmote;
+import net.minecraft.command.server.CommandMessage;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.text.ChatType;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
-import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class LocalChatEventHandler
 {
@@ -36,16 +37,39 @@ public class LocalChatEventHandler
 		event.setCanceled(true);
 	}
 
+
 	@SubscribeEvent
-	@SideOnly(Side.CLIENT)
-	public static void onChatReceived(ClientChatReceivedEvent event)
+	public static void onCommandSent(CommandEvent event)
 	{
-		if(MinestuckConfig.localizedChat && Minecraft.getMinecraft().player != null)
+		if(!MinestuckConfig.localizedChat)
+			return;
+
+		World world = event.getSender().getEntityWorld();
+		EntityPlayer player = world.getPlayerEntityByUUID(event.getSender().getCommandSenderEntity().getUniqueID());
+		boolean canSenderGab = player.getCapability(MinestuckCapabilities.GOD_TIER_DATA, null) != null && player.getCapability(MinestuckCapabilities.GOD_TIER_DATA, null).isBadgeActive(MinestuckBadges.GIFT_OF_GAB);
+
+		if(!canSenderGab && event.getCommand() instanceof CommandEmote)
 		{
-			EntityPlayer player = Minecraft.getMinecraft().player;
-			if(event.getType() == ChatType.SYSTEM && player.getCapability(MinestuckCapabilities.GOD_TIER_DATA, null) != null &&
-					!(player.getCapability(MinestuckCapabilities.GOD_TIER_DATA, null).isBadgeActive(MinestuckBadges.GIFT_OF_GAB) || player.isCreative()))
+			TextComponentTranslation msg = new TextComponentTranslation("commands.generic.permission");
+			msg.getStyle().setColor(TextFormatting.RED);
+			event.getSender().sendMessage(msg);
+			event.setCanceled(true);
+		}
+		else if(event.getCommand() instanceof CommandMessage)
+		{
+			EntityPlayer receiver;
+
+			try { receiver = CommandBase.getPlayer(event.getSender().getServer(), event.getSender(), event.getParameters()[0]); }
+			catch (Throwable throwable) { return; }
+
+
+			boolean canReceiverGab = receiver.getCapability(MinestuckCapabilities.GOD_TIER_DATA, null) != null && receiver.getCapability(MinestuckCapabilities.GOD_TIER_DATA, null).isBadgeActive(MinestuckBadges.GIFT_OF_GAB);
+
+			if(!(receiver == player || (canSenderGab && canReceiverGab) || (!canSenderGab && player.getDistanceSq(receiver) < 128 && player.world.provider.getDimension() == receiver.world.provider.getDimension())))
 			{
+				TextComponentTranslation msg = new TextComponentTranslation("commands.localizedChat.outOfReach", player.getDisplayName());
+				msg.getStyle().setColor(TextFormatting.RED);
+				event.getSender().sendMessage(msg);
 				event.setCanceled(true);
 			}
 		}
